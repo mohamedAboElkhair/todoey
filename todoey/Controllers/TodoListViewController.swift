9f014a904c6eb0c1e05c6762182ef0e8f6dbf942
Mohamed Abo Elkheir <mohamed741987@yandex.com>
2019-06-30 14:47:50 +0200

    SearchBarDone

--- todoey/Controllers/TodoListViewController.swift
+++ todoey/Controllers/TodoListViewController.swift
@@ -98,12 +98,7 @@ extension TodoListViewController : UISearchBarDelegate{
         loadItems(with :request)
     }
     
-//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-//        let request : NSFetchRequest<Item> = Item.fetchRequest()
-//        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@",searchBar.text!)
-//        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-//        loadItems(with :request)
-    //MARk: go to Original
+    //MARk: go
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchBar.text?.count == 0{
             loadItems()
